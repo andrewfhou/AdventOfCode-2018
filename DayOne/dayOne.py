cafb96154727422ce6a97b4019115ab23e9eaf59
Andrew Hou <andrewfhou@gmail.com>
2018-12-01 13:49:42 -0600

    time in ms, some optimization

--- DayOne/dayOne.py
+++ DayOne/dayOne.py
@@ -1,11 +1,13 @@
 import time
 
+curr_ms = lambda: time.time() * 1000
+
 file = open('input.txt', 'r')
 inputs = file.read().strip().split()
 file.close()
 
 def sum() :
-    start = time.time()
+    start = curr_ms()
     freq = 0
 
     for i in inputs: 
@@ -15,10 +17,10 @@ def sum() :
             freq = freq - int(i[1:])
     
     print("Part One: " + str(freq))
-    print("time taken: " + str(time.time() - start))
+    print("time taken: " + str(curr_ms() - start) + "ms")
 
 def solve() :
-    start = time.time()
+    start = curr_ms()
     freq = 0
     seen = set([freq])
     done = False
@@ -26,20 +28,17 @@ def solve() :
 
     while not done :
         for i in inputs :
-            iterTime = time.time()
-            if i[0] == '+' : 
-                freq = freq + int(i[1:])
-            elif i[0] == '-' :
-                freq = freq - int(i[1:])
+            iterTime = curr_ms()
+            freq = freq + int(i[:])
         
             if freq in seen :
-                print("\nPart Two: " + str(freq) + "\ntime taken: " + str(time.time() - start) + "\n")
+                print("\nPart Two: " + str(freq) + "\ntime taken: " + str(curr_ms() - start) + " ms\n")
                 done = True
                 return
             seen.add(freq)
 
-        elapsed = time.time() - iterTime
-        print("iteration " + str(iteration) + "\tfreq: " + str(freq) + "\ttime: " + str(elapsed))
+        elapsed = curr_ms() - iterTime
+        print("iteration " + str(iteration) + "\tfreq: " + str(freq) + "\ttime: " + str(elapsed) + "ms")
         iteration = iteration + 1
 
 solve()
