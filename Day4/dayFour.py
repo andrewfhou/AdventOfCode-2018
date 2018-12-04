871642c43edfd30b2c5bd9c049db985b02d9a9aa
Andrew Hou <andrewfhou@gmail.com>
2018-12-04 01:20:10 -0600

    help

--- Day4/dayFour.py
+++ Day4/dayFour.py
@@ -1,34 +1,47 @@
-import time
-
-
-def curr_ms(): return time.time() * 1000
+from collections import defaultdict, Counter
 
 
 with open("input.txt") as file:
     inputs = file.read().splitlines()
 
+i = 0
+for x in inputs:
+    inputs[i] = x.split(' ')
+    inputs[i][0] = (inputs[i][0])[1:]
+    inputs[i][1] = (inputs[i][1])[:-1]
+    inputs[i][1] = (inputs[i][1])[3:]
+    i += 1
+
+inputs.sort(key=lambda x: ((x[1])[0:2] + (x[1])[3:5]))
+inputs.sort(key=lambda x: x[0])
+
+guards = defaultdict(int)
+timeslots = defaultdict(int)
+
+for x in inputs:
+    timet = int(x[1])
+    if x[2] == 'Guard':
+        guardID = int(x[3][1:])
+    elif x[2] == 'falls':
+        asleep = timet
+    elif x[2] == 'wakes':
+        for tt in range(asleep, timet):
+            guards[(guardID, tt)] += 1
+            timeslots[guardID] += 1
+
 
 def partOne():
-    start = curr_ms()
-
-    i = 0
-    for x in inputs:
-        inputs[i] = x.split(" ")
-        i += 1
+    target = None
+    for key, val in guards.items():
+        if target is None or val > guards[target]:
+            target = key
     
-    inputs.sort(key = lambda x:((x[1])[0:2] + (x[1])[3:5]))
-    inputs.sort(key = lambda x:x[0])
-
-    for x in inputs:
-        print(x)
-
-    print("Time Taken: " + str(curr_ms() - start) + "ms\n")
-
+    targetGuard, targetMin = target
+    print(targetGuard, targetMin)
+    print(targetGuard * targetMin)
 
 def partTwo():
-    start = curr_ms()
-
-    print("Time Taken: " + str(curr_ms() - start) + "ms\n")
+    return
 
 
 partOne()
