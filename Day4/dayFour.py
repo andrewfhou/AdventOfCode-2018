e7418285010b870ad84eff9241f86fc6763540c3
Andrew Hou <andrewfhou@gmail.com>
2018-12-03 23:25:07 -0600

    sorted input

--- Day4/dayFour.py
+++ Day4/dayFour.py
@@ -11,6 +11,17 @@ with open("input.txt") as file:
 def partOne():
     start = curr_ms()
 
+    i = 0
+    for x in inputs:
+        inputs[i] = x.split(" ")
+        i += 1
+    
+    inputs.sort(key = lambda x:((x[1])[0:2] + (x[1])[3:5]))
+    inputs.sort(key = lambda x:x[0])
+
+    for x in inputs:
+        print(x)
+
     print("Time Taken: " + str(curr_ms() - start) + "ms\n")
 
 
